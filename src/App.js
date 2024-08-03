8d263c8a32ac043034d462e9c61f52704c7b9404
EllaLiu0401 <ellatong0515@gmail.com>
2024-08-03 22:58:52 +1000

    feat: listening to a keypress to close the move info

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const average = (arr) =>
 const KEY = "bb6d25ce";
 
 export default function App() {
-  const [query, setQuery] = useState("inception");
+  const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
@@ -30,6 +30,7 @@ export default function App() {
   function handleDeleteWatched(id) {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
   }
+
   useEffect(
     function () {
       const controller = new AbortController();
@@ -48,8 +49,8 @@ export default function App() {
           setMovies(data.Search);
           setError("");
         } catch (error) {
-          console.error(error.message);
           if (error.name !== "AbortError") {
+            console.log(error.message);
             setError(error.message);
           }
         } finally {
@@ -61,6 +62,7 @@ export default function App() {
         setError("");
         return;
       }
+      handleCloseMovie();
       fetchMovies();
       return function () {
         controller.abort();
@@ -230,6 +232,21 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     onAddWatched(newWatchedMovie);
     onCloseMovie();
   }
+
+  useEffect(
+    function () {
+      function callback(e) {
+        if (e.code === "Escape") {
+          onCloseMovie();
+        }
+      }
+      document.addEventListener("keydown", callback);
+      return function () {
+        document.removeEventListener("keydown", callback);
+      };
+    },
+    [onCloseMovie]
+  );
 
   useEffect(
     function () {
